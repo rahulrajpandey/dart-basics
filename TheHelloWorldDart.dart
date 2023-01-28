fad08fb7eb7f4e3349fb9e10db6605869fb3e834
rahulrajpandey <rahulrajpandey0448@gmail.com>
2023-01-28 21:08:58 +0530

    Hello World Program in Dart

---
+++ TheHelloWorldDart.dart
@@ -0,0 +1,9 @@
+// First Program in Dart to get started.
+// Requirements: Dart sdk should be installed.
+
+void main() {
+  print("Hello World!");
+}
+
+// save the file: TheHelloWorldDart.dart
+// run the file: dart TheHelloWorldDart.dart
